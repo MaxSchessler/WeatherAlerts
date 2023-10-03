ec5384cbfaac86ce8b50f7646162d01c78adea63
Max Schessler <schessler.max@gmail.com>
2023-10-03 10:52:32 -0500

    added texting functionality

---
+++ alerts/alerts.py
@@ -0,0 +1,17 @@
+import os
+from twilio.rest import Client
+
+def send_message(message_body, too_phone):
+	account_sid = "AC81e5482d34d6d7b1fbd5982d591edf09"
+	auth_token = "83438458c10dfede95ff561ba9a0f600"
+	from_phone = "+18442094871"
+
+	client = Client(account_sid, auth_token)
+
+	message = client.messages.create(
+	body=message_body,
+	from_=from_phone,
+	to=too_phone
+)
+
+send_message("Test", "+14023264360")
